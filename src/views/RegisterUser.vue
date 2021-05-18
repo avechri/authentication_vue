51e39dc478ba1276393e7c9295e50325e59080c5
kristina <christychri0@gmail.com>
2021-05-18 15:53:53 +0700

    added a router to dashboard after login

--- src/views/RegisterUser.vue
+++ src/views/RegisterUser.vue
@@ -42,6 +42,9 @@ export default {
         email: this.email,
         password: this.password
       })
+        .then(() => {
+          this.$router.push({ name: 'dashboard' })
+        })
     }
   }
 }
