a172fb527c4a5693ffe9f796fe8f352e93c4248b
kristina <christychri0@gmail.com>
2021-05-19 14:58:49 +0700

    added an auto-login after refreshing page

--- src/main.js
+++ src/main.js
@@ -8,5 +8,14 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
+  created () {
+    // after app created, we're checking if there is user data & if it is, we use vuex to restore it
+    const userString = localStorage.getItem('user')
+
+    if (userString) {
+      const userData = JSON.parse(userString)
+      this.$store.commit('SET_USER_DATA', userData)
+    }
+  },
   render: h => h(App)
 }).$mount('#app')
